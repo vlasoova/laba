2e7501c1bf813d3b6e23d5456b6e6c5bcc397861
a <a@a.a>
2024-01-09 16:44:20 +0300

    Added space to M.java

--- src/humans/Margory.java
+++ src/humans/Margory.java
@@ -6,6 +6,9 @@ public class Margory extends Human implements See {
     public Margory(String name, String surname){
         super(name, surname);
     }
+
+
+
     public void see(Human human){
         System.out.printf("%n%s %s saw %s was going along the street.", getName(), getSurname(), human.getName());
     }
